c47a04e410647a8937221ef657febfc7840db14e
desinoviyanti53 <desinoviyanti53@gmail.com>
2024-05-31 10:28:34 +0700

    ProjectDesi

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// tambah dibawah ini
+Route::resource('destinasi', App\Http\Controllers\DestinasiController::class)->middleware('auth');
+Route::resource('pengunjung', App\Http\Controllers\PengunjungController::class)->middleware('auth');
+Route::resource('reservasi', App\Http\Controllers\ReservasiController::class)->middleware('auth');
+Route::resource('transaksi', App\Http\Controllers\TransaksiController::class)->middleware('auth');
